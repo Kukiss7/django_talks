e09499d51118234dbfe250adda11b9db12c59366
Łukasz Janicki <janicki_lukasz@o2.pl>
2018-09-08 22:16:10 +0200

    updates to name 'Django Talks'

--- django_one/settings.py
+++ django_one/settings.py
@@ -88,6 +88,11 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 
 
 # Password validation
